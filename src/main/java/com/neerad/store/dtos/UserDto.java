c3ed52f90da37e643788d51ec1808f10c58ea37d
NEERAD <neerad_2024bcse079@nitsri.ac.in>
2026-03-09 06:04:59 +0530

    Customizing Response Data

--- src/main/java/com/neerad/store/dtos/UserDto.java
+++ src/main/java/com/neerad/store/dtos/UserDto.java
@@ -1,12 +1,26 @@
 package com.neerad.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+//    @JsonIgnore // use to exclude a field from JSON output
+    @JsonProperty("user_id")// used to rename a field
     private Long id;
     private String name;
     private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+//    @JsonInclude(JsonInclude.Include.NON_NULL) // used to hide fields which have null value
+//    private Long phoneNumber;
+
+
 }
--- src/main/java/com/neerad/store/mappers/UserMapper.java
+++ src/main/java/com/neerad/store/mappers/UserMapper.java
@@ -3,9 +3,11 @@ package com.neerad.store.mappers;
 import com.neerad.store.dtos.UserDto;
 import com.neerad.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target= "createdAt" ,expression = "java(java.time.LocalDateTime.now())")
     // Maps object automatically during compile time , no need to map object manually
     UserDto toDto(User user);
 }
